112be5848a8fba0bdde0d4917f16d983fbf1ea6e
Rohan Roberts <rohan.a.roberts@gmail.com>
2026-02-20 21:30:57 +0000

    Manual code change

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
+// Shopify App Bridge requires the PUBLIC API key (Client ID)
+window.__SHOPIFY_API_KEY__ = '67be6ef7574f3a32bf9a218ad4582c68'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
