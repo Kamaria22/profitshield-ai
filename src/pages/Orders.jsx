45112bed65cdf3ef62c2a1ee98dd90f63120ba59
Rohan Roberts <rohan.a.roberts@gmail.com>
2026-03-02 22:48:38 +0000

    File changes

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -39,6 +39,7 @@ import { createPageUrl } from '@/components/platformContext';
 
 import OrdersTable from '../components/orders/OrdersTable';
 import OrderDetailPanel from '../components/orders/OrderDetailPanel';
+import OrderSearchBox from '../components/orders/OrderSearchBox';
 import DebugBanner from '../components/DebugBanner';
 
 export default function Orders() {
