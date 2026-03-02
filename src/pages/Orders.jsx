c0439ba99952c9221571bba0c0f601e5a7890233
Rohan Roberts <rohan.a.roberts@gmail.com>
2026-03-02 22:49:15 +0000

    File changes

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -383,15 +383,11 @@ export default function Orders() {
 
       {/* Filters */}
       <div className="flex flex-col sm:flex-row gap-3">
-        <div className="relative flex-1">
-          <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-slate-400" />
-          <Input
-            placeholder="Search orders..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="pl-9"
-          />
-        </div>
+        <OrderSearchBox
+          value={searchTerm}
+          onChange={setSearchTerm}
+          orders={orders}
+        />
 
         <Select 
           value={filters.dateRange} 
